51ee7fe735d29b6c3a633c2ce0be31dbf5792aee
Matvei <115184232+DeadInsideDi@users.noreply.github.com>
2026-06-07 14:09:08 +0300

    controller v1.2

--- controller.lua
+++ controller.lua
@@ -4,40 +4,77 @@ return (function()
   local VirtualInputManager = game:GetService("VirtualInputManager")
   local GuiService = game:GetService("GuiService")
 
-  local GuiInset = GuiService:GetGuiInset()
-  local GuiInsetX = GuiInset.X
-  local GuiInsetY = GuiInset.Y
-  local InputTimeInterval = 0.04
+  local MoveTimeInterval = 0.01
+  local ClickTimeInterval = 0.04
+  local TapTimeInterval = 0.03
+  local ScrollTimeInterval = 0.02
+  local TypeTimeInterval = 0.01
 
-  function Controller.Click(x: number, y: number, withInset: boolean?): ()
-    if withInset == nil then withInset = true end
-    local TargetX = x + (if withInset then GuiInsetX else 0)
-    local TargetY = y + (if withInset then GuiInsetY else 0)
+  function Controller.Move(dx: number, dy: number): ()
+    VirtualInputManager:SendMouseMoveDeltaEvent(dx, dy, game)
+    task.wait(MoveTimeInterval)
+  end
 
-    VirtualInputManager:SendMouseMoveEvent(TargetX, TargetY, game)
-    task.wait(InputTimeInterval)
-    VirtualInputManager:SendMouseButtonEvent(TargetX, TargetY, 0, true, game, 0)
-    task.wait(InputTimeInterval)
-    VirtualInputManager:SendMouseButtonEvent(TargetX, TargetY, 0, false, game, 0)
-    task.wait(InputTimeInterval)
+
+  function Controller.Click(x: number, y: number): ()
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, 0)
+    task.wait(ClickTimeInterval)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, 0)
+    task.wait(ClickTimeInterval)
   end
 
   function Controller.ClickGui(guiElement: GuiObject): ()
     local TargetX = guiElement.AbsolutePosition.X + (guiElement.AbsoluteSize.X / 2)
     local TargetY = guiElement.AbsolutePosition.Y + (guiElement.AbsoluteSize.Y / 2)
+    TargetX += GuiService:GetGuiInset().X
+    TargetY += GuiService:GetGuiInset().Y
+
     Controller.Click(TargetX, TargetY)
   end
 
   function Controller.Tap(keyCode: Enum.KeyCode): ()
     VirtualInputManager:SendKeyEvent(true, keyCode, false, game)
-    task.wait(InputTimeInterval)
+    task.wait(TapTimeInterval)
     VirtualInputManager:SendKeyEvent(false, keyCode, false, game)
-    task.wait(InputTimeInterval)
+    task.wait(TapTimeInterval)
   end
+
+  function Controller.Scroll(scrollDelta: number, x: number?, y: number?): ()
+    VirtualInputManager:SendMouseWheelEvent(x or 0, y or 0, scrollDelta, game)
+    task.wait(ScrollTimeInterval)
+  end
+
+  function Controller.Type(character: string): ()
+    VirtualInputManager:SendTextInputCharacterEvent(character, game)
+    task.wait(TypeTimeInterval)
+  end
+
+  function Controller.SetMoveTimeInterval(value: number): ()
+    MoveTimeInterval = type(value) == "number" and value or MoveTimeInterval
+  end
+
+  function Controller.SetClickTimeInterval(value: number): ()
+    ClickTimeInterval = type(value) == "number" and value or ClickTimeInterval
+  end
+
+  function Controller.SetTapTimeInterval(value: number): ()
+    TapTimeInterval = type(value) == "number" and value or TapTimeInterval
+  end
+
+  function Controller.SetScrollTimeInterval(value: number): ()
+    ScrollTimeInterval = type(value) == "number" and value or ScrollTimeInterval
+  end
+
+  function Controller.SetTypeTimeInterval(value: number): ()
+    TypeTimeInterval = type(value) == "number" and value or TypeTimeInterval
+  end
+
 
   return Controller
 end)()
 -- Controller = loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadInsideDi/lua/main/controller.lua"))()
 
--- click(number, number, withInset=True) / clickGui(GuiObject)
--- tap(Enum.KeyCode)
+-- Move(number, number) / Click(number, number) / ClickGui(GuiObject)
+-- Tap(Enum.KeyCode) / Scroll(number, number?, number?) / Type(string)
+-- SetMoveTimeInterval(number) / SetClickTimeInterval(number) / SetTapTimeInterval(number)
+-- SetScrollTimeInterval(number) / SetTypeTimeInterval(number)
