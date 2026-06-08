cfe306832316bfcfa70d4ab7df87853a3d685dae
Matvei <115184232+DeadInsideDi@users.noreply.github.com>
2026-06-08 18:45:54 +0300

    v2.0

--- controller.lua
+++ controller.lua
@@ -4,23 +4,27 @@ return (function()
   local VirtualInputManager = game:GetService("VirtualInputManager")
   local GuiService = game:GetService("GuiService")
 
-  local MoveTimeInterval = 0.01
-  local ClickTimeInterval = 0.04
-  local TapTimeInterval = 0.03
-  local ScrollTimeInterval = 0.02
-  local TypeTimeInterval = 0.01
+  if not getgenv().CreateCustomValue then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadInsideDi/lua/main/createcustomvalue.lua"))()
+  end
+  local CreateValue = getgenv().CreateCustomValue
+
+  Controller.MoveTimeInterval = CreateValue(0.01)
+  Controller.ClickTimeInterval = CreateValue(0.04)
+  Controller.TapTimeInterval = CreateValue(0.03)
+  Controller.ScrollTimeInterval = CreateValue(0.02)
+  Controller.TypeTimeInterval = CreateValue(0.01)
 
   function Controller.Move(dx: number, dy: number): ()
     VirtualInputManager:SendMouseMoveDeltaEvent(dx, dy, game)
-    task.wait(MoveTimeInterval)
+    task.wait(Controller.MoveTimeInterval)
   end
-
 
   function Controller.Click(x: number, y: number): ()
     VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, 0)
-    task.wait(ClickTimeInterval)
+    task.wait(Controller.ClickTimeInterval)
     VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, 0)
-    task.wait(ClickTimeInterval)
+    task.wait(Controller.ClickTimeInterval)
   end
 
   function Controller.ClickGui(guiElement: GuiObject): ()
@@ -34,41 +38,20 @@ return (function()
 
   function Controller.Tap(keyCode: Enum.KeyCode): ()
     VirtualInputManager:SendKeyEvent(true, keyCode, false, game)
-    task.wait(TapTimeInterval)
+    task.wait(Controller.TapTimeInterval)
     VirtualInputManager:SendKeyEvent(false, keyCode, false, game)
-    task.wait(TapTimeInterval)
+    task.wait(Controller.TapTimeInterval)
   end
 
   function Controller.Scroll(scrollDelta: number, x: number?, y: number?): ()
     VirtualInputManager:SendMouseWheelEvent(x or 0, y or 0, scrollDelta, game)
-    task.wait(ScrollTimeInterval)
+    task.wait(Controller.ScrollTimeInterval)
   end
 
   function Controller.Type(character: string): ()
     VirtualInputManager:SendTextInputCharacterEvent(character, game)
-    task.wait(TypeTimeInterval)
+    task.wait(Controller.TypeTimeInterval)
   end
-
-  function Controller.SetMoveTimeInterval(value: number): ()
-    MoveTimeInterval = type(value) == "number" and value or MoveTimeInterval
-  end
-
-  function Controller.SetClickTimeInterval(value: number): ()
-    ClickTimeInterval = type(value) == "number" and value or ClickTimeInterval
-  end
-
-  function Controller.SetTapTimeInterval(value: number): ()
-    TapTimeInterval = type(value) == "number" and value or TapTimeInterval
-  end
-
-  function Controller.SetScrollTimeInterval(value: number): ()
-    ScrollTimeInterval = type(value) == "number" and value or ScrollTimeInterval
-  end
-
-  function Controller.SetTypeTimeInterval(value: number): ()
-    TypeTimeInterval = type(value) == "number" and value or TypeTimeInterval
-  end
-
 
   return Controller
 end)()
@@ -76,5 +59,5 @@ end)()
 
 -- Move(number, number) / Click(number, number) / ClickGui(GuiObject)
 -- Tap(Enum.KeyCode) / Scroll(number, number?, number?) / Type(string)
--- SetMoveTimeInterval(number) / SetClickTimeInterval(number) / SetTapTimeInterval(number)
--- SetScrollTimeInterval(number) / SetTypeTimeInterval(number)
+-- MoveTimeInterval: number / ClickTimeInterval: number / TapTimeInterval: number
+-- ScrollTimeInterval: number / TypeTimeInterval: number
