7bc731756d60847bbed93f630ee54417d97f73f1
Matvei <115184232+DeadInsideDi@users.noreply.github.com>
2026-06-13 12:33:21 +0300

    Update characterfinder.lua

--- characterfinder.lua
+++ characterfinder.lua
@@ -1,18 +1,27 @@
 return (function ()
   local RunService = game:GetService("RunService")
   local Camera = workspace.CurrentCamera
+  local Players = game:GetService("Players")
+  local Client = Players.LocalPlayer
+
   getgenv().CharacterFinderRunned = true
 
   RunService:UnbindFromRenderStep("CharacterFinder")
   RunService:BindToRenderStep("CharacterFinder", Enum.RenderPriority.Last.Value, function()
+    if Client.Character then
+      getgenv().Character = Client.Character
+      return
+    end
     local Counts, MaxCount, PossibleCharacter = {}, 0, nil
-    local Parts = workspace:GetPartBoundsInRadius(Camera.CFrame.Position, 0.1)
-    local FocusParts = workspace:GetPartBoundsInRadius(Camera.Focus.Position, 0.1)
+    local Parts = workspace:GetPartBoundsInRadius(Camera.CFrame.Position, 0.01)
+    local FocusParts = workspace:GetPartBoundsInRadius(Camera.Focus.Position, 0.01)
     table.move(FocusParts, 1, #FocusParts, #Parts + 1, Parts)
 
     for _, Part in Parts do
-      local Model = Part:FindFirstAncestorOfClass("Model")
-      if Model then Counts[Model] = (Counts[Model] or 0) + 1 end
+      if Part.Size.Magnitude < 10 then
+        local Model = Part:FindFirstAncestorOfClass("Model")
+        if Model then Counts[Model] = (Counts[Model] or 0) + 1 end
+      end
     end
 
     for Model, Count in Counts do
@@ -21,6 +30,7 @@ return (function ()
         PossibleCharacter = Model
       end
     end
+
     getgenv().Character = PossibleCharacter
   end)
 end)()
